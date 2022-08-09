70b8c82dca92b18ff3e19c64d889a1b08c577aa8
Sants16 <jvsantosrocha980@gmail.com>
2022-08-09 16:23:11 -0300

    try-catch

---
+++ js/debugging-e-error-handling/try-catch.js
@@ -0,0 +1,16 @@
+function verificarPalindromo(string){
+    if(!string) throw 'String inválida'
+
+    return string === string.split('').reverse().join('')
+}
+
+function tryCatchExemplo(string){
+    try{
+        verificarPalindromo(string) //verifica o código e se ele tiver algum tipo de erro ele sera tratado no catch
+    }
+    catch(e){
+        console.log(e) //trata o erro derivado da função
+    }
+}
+
+tryCatchExemplo('')
