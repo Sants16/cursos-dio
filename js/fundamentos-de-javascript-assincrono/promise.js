73793750557205be130d3fcc76264b90f6b6f55f
Sants16 <jvsantosrocha980@gmail.com>
2022-08-14 17:55:58 -0300

    promise/async-await

---
+++ js/fundamentos-de-javascript-assincrono/promise.js
@@ -0,0 +1,16 @@
+//ESTRUTURA
+const myPromise = new Promise((resolve, reject) => {
+    //depois de 2 segundos que a promise for recebida quando ela resolvida será exibido no console 'Resolvida!'
+    setTimeout(() => {
+        resolve('Resolvida!')
+    }, 2000)
+})
+
+//MANIPULAÇÃO
+await myPromise //.then() é o que fazemos depois da promise ser resolvida
+            .then((result) => `${result} passando pelo then`)
+            .then((result) => `${result} e agora acabou!`)
+            .catch((err) => console.log(err.message))
+//Após 2 segundos, irá retorna "Resolvida passando pelo then e agora acabou"
+
+//TESTA O EXEMPLO NO CONSOLE DO NAVEGADOR
