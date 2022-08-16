8ceef77d4809b8d9d9c05ac639c42e6905baf8c4
Sants16 <jvsantosrocha980@gmail.com>
2022-08-15 21:22:37 -0300

    no-cors

--- js/fundamentos-de-javascript-assincrono/catAPI/assets/js/scripts.js
+++ js/fundamentos-de-javascript-assincrono/catAPI/assets/js/scripts.js
@@ -1,16 +1,15 @@
-const BASE_URL = 'https://thatcopy.pw/catapi/rest/' //url de acesso da nossa api
+const BASE_URL = 'https://thatcopy.pw/catapi/rest/'; //url de acesso da nossa api
 const catBtn = document.querySelector('#change-cat') //referencia o botão que ira mudar a imagem dos gatos no html
 const catImg = document.querySelector('#cat') //referencia o tag img que ira exibir a imagem dos gatos no html
 
 const getCats = async () => { //função assincrona que ira esperar a resposta da promise do metodo fetch
     try { //tenta executar o seguinte código:
-        const data = await fetch(BASE_URL) //define o 'data' como o resultado da requisição com a BASE_URL
+        const data = await fetch(BASE_URL, { mode: "no-cors", }) //define o 'data' como o resultado da requisição com a BASE_URL
         const json = await data.json() //converte os dados recebidos para .json e os armazena dentro da const json
-
         return json.webpurl //retorna o atributo webpurl do object json
     }
     catch(error) { //caso der erro ele sera exibido no console.log
-        console.log(error.message)
+        console.log(error)
     }
 }
 
