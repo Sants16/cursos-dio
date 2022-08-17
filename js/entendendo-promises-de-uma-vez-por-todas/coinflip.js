8db75e9f2bff064d64211048b7a3eb308b517193
Sants16 <jvsantosrocha980@gmail.com>
2022-08-17 18:00:36 -0300

    coinFlip-HandlingCatchs

--- js/entendendo-promises-de-uma-vez-por-todas/coinflip.js
+++ js/entendendo-promises-de-uma-vez-por-todas/coinflip.js
@@ -1,7 +1,8 @@
 const coinFlip = new Promise((resolve, reject) => Math.random() > 0.5 ? resolve('Resolvido') : reject('Rejeitado'))
 // definimos que a Promise será tratada como resolvida (resolve) quando o número gerado for maior que 0.5 nessas condições a Promise irá retornar a string 'Resolvido', caso o número gerado seja menor que 0.5 a Promise será tratada como rejeitada (reject) e irá retornar a string 'Rejeitado'
 
-coinFlip //aqui estamos manipulando os estados da Promise
-    .then(data => console.log(data, '1')) //se o estado da Promise for resolve iremos pegar a string retornada e exibila no console junto com o número 1
-    .then(() => console.log('Fim')) //apos o primeiro .then() ser executado iremos exibir 'Fim' no console
-    .catch(err => console.log(err, 'Erro 1')) //se o estado da Promise for reject iremso pegar a string retornada e exibila no console junto com a mensagem 'Erro 1'
+coinFlip.then(data => console.log('Primeiro then'))
+    .catch((err) => console.log('Primeiro catch')) //so sera executado se o primeiro then der erro
+    .then(() => console.log('Segundo then'))
+    .catch((err) => console.error('Erro pego do segundo then')) //so sera executado se o segundo then der erro
+    .then(() => console.log('Fim')) //vai ser executado sempre pois o catch não para a execução do código
