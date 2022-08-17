9b9d88eadfc545fbea9de6d131bef20bbf7ebb20
Sants16 <jvsantosrocha980@gmail.com>
2022-08-17 18:05:36 -0300

    catchThrowError

--- js/entendendo-promises-de-uma-vez-por-todas/coinflip.js
+++ js/entendendo-promises-de-uma-vez-por-todas/coinflip.js
@@ -2,7 +2,7 @@ const coinFlip = new Promise((resolve, reject) => Math.random() > 0.5 ? resolve(
 // definimos que a Promise será tratada como resolvida (resolve) quando o número gerado for maior que 0.5 nessas condições a Promise irá retornar a string 'Resolvido', caso o número gerado seja menor que 0.5 a Promise será tratada como rejeitada (reject) e irá retornar a string 'Rejeitado'
 
 coinFlip.then(data => console.log('Primeiro then'))
-    .catch((err) => console.log('Primeiro catch')) //so sera executado se o primeiro then der erro
-    .then(() => console.log('Segundo then'))
+    .catch(() => console.log('Primeiro catch')) //so sera executado se o primeiro then der erro
+    .then(() => { throw new Error() })
     .catch((err) => console.error('Erro pego do segundo then')) //so sera executado se o segundo then der erro
     .then(() => console.log('Fim')) //vai ser executado sempre pois o catch não para a execução do código
